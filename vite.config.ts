f1da8479a98e59e96760078d56ae60ad72c24490
kay delaney <kay@grafana.com>
2024-07-19 17:35:11 +0100

    Move output to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'docs'
+      }
+    }
+  }
 })
